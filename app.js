e8322297b3990470421f79cda3c66fbb7cdab5aa
Germán <germanmacchia@gmail.com>
2020-12-27 23:06:52 -0300

    nuevos metodos incluidos

--- app.js
+++ app.js
@@ -148,14 +148,10 @@ app.get('/categoria', async (req, res) => {  //Se espera la respuesta antes de s
 	try {
 		const query = 'SELECT * FROM genero' //Consulta MySQL
 		const respuesta = await qy(query); 
-		console.log(respuesta);
-		// async de la consulta
 		res.status(200).send({
-			'respuesta': respuesta           //se manda JSON 
+			'respuesta': respuesta           // Devuelve JSON 
 		});
-		//conexion.query(query);
 	} catch (e) {
-		// statements
 		console.log(e.message);
 		res.status(413).send({
 			'error': e.message
@@ -168,10 +164,131 @@ app.get('/categoria', async (req, res) => {  //Se espera la respuesta antes de s
 //PERSONA--------------------------
 
 
+	/* GET '/persona' retorna status 200 y [{id: numerico, nombre: string, apellido: string, alias: string, email; string}] o bien status 
+	413 y [] */
+
+app.get('/personas', async (req, res) => {
+	try {
+		const query = 'SELECT * FROM personas' //Consulta MySQL
+		const respuesta = await qy(query);
+		res.status(200).send({
+			'respuesta': respuesta //devuelve JSON 
+		});
+	} catch (e) {
+		console.log(e.message);
+		res.status(413).send({
+			'error': e.message
+		});
+	}
+});
+
 	/* POST '/persona' recibe: {nombre: string, apellido: string, 
 	alias: string, email: string} retorna: status: 200, 
-	{id: numerico, nombre:
- 	string, apellido: string, alias: string, email: string} - 
+	{id: numerico, nombre: string, apellido: string, alias: string, email: string} - 
  	status: 413, {mensaje: <descripcion del error>} que puede ser: "faltan 
 	datos", "el email ya se encuentra registrado", "error inesperado" */
 
+app.post('/personas', async (req, res) => { //Se espera la respuesta antes de seguir con el programa 
+	try {
+		if (!req.body.email || !req.body.apellido || !req.body.nombre || !req.body.alias) { //Validación de envio correcto de informacion
+			throw new Error('Faltan datos'); //Si no hay JSON en el body tira error
+		}
+
+		const email = req.body.email.toUpperCase();
+		const apellido = req.body.apellido.toUpperCase();
+		const nombre = req.body.nombre.toUpperCase();
+		const alias = req.body.alias.toUpperCase();
+
+		//Verifico que no exista previamente esa persona a través del email
+		let query = 'SELECT id_persona FROM personas WHERE email_persona = ?';
+		let respuesta = await qy(query, [email]);
+
+		if (respuesta.length > 0) { //si no me arroja ningun resultado entonces el query esta vacio
+			throw new Error('El email ya se encuentra registrado')
+		}
+
+		//Guardo los datos
+		query = 'INSERT INTO personas (nombre_persona, apellido_persona, email_persona, alias_persona) VALUES (?,?,?,?)';
+		respuesta = await qy(query, [nombre, apellido, email, alias]);
+
+		res.status(200).send({
+			"Id": respuesta.insertId,
+			"Nombre": nombre,
+			"Apellido": apellido,
+			"Email": email,
+			"Alias": alias
+		})
+
+	} catch (e) {
+		// statements
+		console.log(e.message);
+		res.status(413).send({
+			'error': e.message
+		});
+	}
+});
+
+//LIBRO-----------------------------
+
+	/*POST '/libro' recibe: {nombre:string, descripcion:string, 
+	categoria_id:numero, persona_id:numero/null} devuelve 200 y 
+	{id: numero, nombre:string, descripcion:string, categoria_id:numero,
+	persona_id:numero/null} o bien status 413,  {mensaje: <descripcion del error>} 
+	que puede ser "error inesperado", "ese libro ya existe", 
+	"nombre y categoria son datos obligatorios", "no existe la categoria indicada", 
+	"no existe la persona indicada"*/
+
+app.post('/libro', async (req, res) => { //Se espera la respuesta antes de seguir con el programa 
+	try {
+		if (!req.body.nombre || !req.body.descripcion || !req.body.categoria_id) { //Validación de envio correcto de informacion
+			throw new Error('Nombre y Categoría son datos obligatorios'); //Si no hay JSON en el body tira error
+		}
+
+		const nombre = req.body.nombre.toUpperCase(); //Funcion para estandarizarla en mayusculas
+		
+		//Verifico que no exista previamente el libro
+		let query = 'SELECT nombre_libro FROM libros WHERE nombre_libro = ?';
+		let respuesta = await qy(query, [nombre]);
+
+		if (respuesta.length > 0) { //si no me arroja ningun resultado entonces el query esta vacio
+			throw new Error('Ese libro ya existe')
+		}
+
+		//Verifico que exista previamente esa categoria
+		query = 'SELECT id_categoria FROM genero WHERE id_categoria = ?';
+		respuesta = await qy(query, [req.body.categoria_id]);
+
+		if (respuesta.length == 0) { //si no me arroja ningun resultado entonces el query esta vacio
+			throw new Error('No existe la categoria indicada')
+		}
+		
+		if (req.body.persona_id != null){
+			//Verifico que exista previamente la persona
+			query = 'SELECT id_persona FROM personas WHERE id_persona = ?';
+			respuesta = await qy(query, [req.body.persona_id]);
+
+			if (respuesta.length == 0) { //si no me arroja ningun resultado entonces el query esta vacio
+				throw new Error('No existe la persona indicada')
+			}
+		}	
+
+		//Guardo el nuevo libro
+		query = 'INSERT INTO libros (nombre_libro, descripcion_libro, id_categoria, id_persona) VALUE (?,?,?,?)';
+		respuesta = await qy(query, [nombre, req.body.descripcion, req.body.categoria_id, req.body.persona_id]);
+
+		res.status(200).send({
+			'Id': respuesta.insertId,
+			'Nombre': nombre,
+			'Descripcion': req.body.descripcion,
+			'Categoria': req.body.categoria_id,
+			'Persona': req.body.persona_id			
+		});
+
+	} catch (e) {
+		// statements
+		console.log(e.message);
+		res.status(413).send({
+			'error': e.message
+		});
+	}
+});	
