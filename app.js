ad416b2f9548379ba27002a35937353bfaf8d467
Germán <germanmacchia@gmail.com>
2021-04-20 20:22:18 -0300

    back

--- app.js
+++ app.js
@@ -25,14 +25,6 @@ app.use((req, res, next) => {
     next();
 });
 
-//Configurar peticiones
-app.use(function (req, res, next) {
-    if (req.headers['x-forwarded-proto'] === 'https') {
-      res.redirect('http://' + req.hostname + req.url);
-    } else {
-      next();
-    }
-  });
 // Autenticación (Middleware) ----------
 
 const auth = (req, res, next) => {
