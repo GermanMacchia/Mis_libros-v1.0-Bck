c2421e7cc861bdc09f94b6fc782022365b62056b
GermanMacchia <69181142+GermanMacchia@users.noreply.github.com>
2022-12-24 17:59:23 -0300

    Update db.js

--- db.js
+++ db.js
@@ -12,7 +12,6 @@ function connectDataBase() {
 				"connectionLimit": 10,
 				"host": process.env.HOST,
 				"user": process.env.USER,
-				"port": process.env.DB_PORT,
 				"password": process.env.PASSWORD,
 				"database": process.env.DB
 			}
