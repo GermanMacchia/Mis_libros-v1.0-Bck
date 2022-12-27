4b03e1592cc9e2b992632058fcff7429e7bb4ced
German O. Macchia <germanmacchia@gmail.com>
2022-12-27 13:06:39 -0300

    persona peticion fix

--- services/personaService.js
+++ services/personaService.js
@@ -3,7 +3,7 @@ const personaModel = require('../models/persona.js');
 module.exports = {
 	nuevaPersona: async (persona) => {
         var respuesta = await personaModel.mailPersona(persona.email);
-        if (respuesta.length > 0) {
+        if (respuesta) {
             throw new Error('El email ya se encuentra registrado')
         }
  
@@ -13,24 +13,22 @@ module.exports = {
 
 	actualizarPersona: async (persona) => {
 		var respuesta = await personaModel.personaId(persona.id_params);
-		if (respuesta.length == 0) {
+		if (respuesta == null) {
             throw new Error('La persona no existe');
         }
 
-        
-       
         respuesta = await personaModel.personaUpdate(persona);
         return respuesta;
 	},
 
 	borrarPersona: async (id) => {
         var respuesta = await personaModel.personaId(id);
-        if (respuesta.length == 0) {
+        if (respuesta == null) {
             throw new Error("Esta persona no se encuentra registrada");
         }
 
         respuesta = await personaModel.librosPersona(id);
-        if (respuesta.length > 0) {
+        if (respuesta) {
             throw new Error(
                 "Esta persona tiene libros asociados, no se puede eliminar"
             );
